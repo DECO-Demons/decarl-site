efa1642bf1d0b3c4f1cef74ab1042c424323d808
Luka Manitta <luka@lukamanitta.com>
2023-10-13 10:10:24 +1000

    feat: adjust navbar & complete hero section

--- src/lib/sections/Hero.svelte
+++ src/lib/sections/Hero.svelte
@@ -1,13 +1,65 @@
 <section id="hero-section">
-    <h1>Decarl</h1>
-    <h2>Welcome to the "decarl" marketing website!</h2>
-    <h3>NOTE! This website is a W.I.P</h3>
+    <div id="tagline-container">
+        <img
+            src="/decarl-logo.png"
+            alt="tagline-logo"
+            id="tagline-logo"
+        />
+        <h3 id="tagline-text" class="box">A new age of digital art</h3>
+    </div>
+    <div id="hero-arts">
+        <img
+            src="/art-world-paint.png"
+            alt="hero-art-painting"
+            class="hero-art"
+        />
+        <img
+            src="/art-texting.png"
+            alt="hero-art-texting"
+            class="hero-art"
+        />
+        <img
+            src="/art-hanging-frame.png"
+            alt="hero-art-hanging-frame"
+            class="hero-art"
+        />
+    </div>
 </section>
 
 <style>
     #hero-section {
-        padding-top: 5rem;
+        display: flex;
+        padding-top: 7rem;
+        flex-direction: column;
         height: 100vh;
-        background: var(--tertiary-100);
+    }
+
+    #tagline-container {
+        margin: 0 auto;
+    }
+
+    #tagline-logo {
+        margin: 0 auto;
+        display: flex;
+        height: 5rem;
+    }
+
+    #tagline-text {
+        background-color: var(--grey-050);
+        padding: 1.5rem;
+        margin-top: -1.5rem;
+    }
+
+    #hero-arts {
+        height: 100%;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        margin: 0 auto;
+        gap: 10rem;
+    }
+
+    .hero-art {
+        height: 15rem;
     }
 </style>
